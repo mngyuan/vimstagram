e9eea4fbc0553079f3478b052cff8a9983b70c2f
Kevin Lee <kevinmlee.x@gmail.com>
2016-02-14 21:10:15 -0800

    support loading more, somewhat

--- src/vimstagram.js
+++ src/vimstagram.js
@@ -4,18 +4,24 @@ $(document).ready(function() {
   $('body').on('keydown', function(e) {
     if (e.keyCode === 74) {
       // J
-      // TODO: click loadmore and decipher the anti click() magic
-      lastImage = Math.min(lastImage + 1, $('article').length - 1);
-
-      $(this).animate({
-        scrollTop: $('article').eq(lastImage).offset().top - 50
-      });
+      lastImage++;
+      if (lastImage === $('article').length) {
+        // if need to load more scroll to bottom
+        lastImage--;
+        $(this).animate({
+          scrollTop: $(document).height()
+        }, 100);
+      } else {
+        $(this).animate({
+          scrollTop: $('article').eq(lastImage).offset().top - 50
+        }, 100);
+      }
     } else if (e.keyCode === 75) {
       // K
       lastImage = Math.max(lastImage - 1, 0);
       $(this).animate({
         scrollTop: $('article').eq(lastImage).offset().top - 50
-      });
+      }, 100);
     } else if (e.keyCode === 76) {
       // TODO: L, like
     }
