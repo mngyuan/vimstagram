0cadad868dff1f03f1511f584f5ff97d2d12cda5
Kevin Lee <kevinmlee.x@gmail.com>
2016-04-11 02:07:34 -0700

    add support for commenting, expanding comments, reporting, and undoing a comment

--- src/vimstagram.js
+++ src/vimstagram.js
@@ -1,8 +1,13 @@
 var lastImage = -1;
 
 $(document).ready(function() {
-  $('body').on('keydown', function(e) {
-    if (e.keyCode === 74) {
+  $('body').on('keypress', function(e) {
+    if (e.target.tagName.toLowerCase() === 'input' ||
+        e.target.tagName.toLowerCase() === 'textarea') {
+      return;
+    }
+
+    if (e.keyCode === 106) {
       // J
       lastImage++;
       if (lastImage === $('article').length) {
@@ -16,15 +21,32 @@ $(document).ready(function() {
           scrollTop: $('article').eq(lastImage).offset().top - 50
         }, 100);
       }
-    } else if (e.keyCode === 75) {
+    } else if (e.keyCode === 107) {
       // K
       lastImage = Math.max(lastImage - 1, 0);
       $(this).animate({
         scrollTop: $('article').eq(lastImage).offset().top - 50
       }, 100);
-    } else if (e.keyCode === 76) {
+    } else if (e.keyCode === 108) {
       // L, like
+      if (lastImage == -1) { return; }
       $('.coreSpriteHeartOpen, .coreSpriteHeartFull')[lastImage].click();
+    } else if (e.keyCode === 99) {
+      if (lastImage == -1) { return; }
+      // C, comment
+      $("input[placeholder='Add a comment…']")[lastImage].focus();
+      e.preventDefault();
+    } else if (e.keyCode === 118) {
+      if (lastImage == -1) { return; }
+      // V, view all comments
+      $('article').eq(0).find('button').not('.coreSpriteEllipsis').click();
+    } else if (e.keyCode === 114) {
+      if (lastImage == -1) { return; }
+      // R, report or embed
+      $('article').eq(0).find('button.coreSpriteEllipsis').click();
+    } else if (e.keyCode === 117) {
+      // U, undo (comment)
+      $('article').eq(0).find('button[title="Delete Comment"]').last().click();
     }
   });
 });
